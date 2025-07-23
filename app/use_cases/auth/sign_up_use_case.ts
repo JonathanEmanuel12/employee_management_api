febffd9c4d5ea3ada9105880fec31805b9a72d86
Jonathan <jonathaanemanuel21@gmail.com>
2025-07-23 19:28:47 -0300

    add email check in the sign up user

--- app/use_cases/auth/sign_up_use_case.ts
+++ app/use_cases/auth/sign_up_use_case.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from "../../dto/user_dto.js";
 import AuthRepository from "../../repositories/auth_repository.js";
 import UserRepository from "../../repositories/user_repository.js";
 import { AccessDto } from "../../dto/auth_dto.js";
+import ApplicationException from "#exceptions/application_exception";
 
 @inject()
 export default class SignUpUseCase {
@@ -12,9 +13,17 @@ export default class SignUpUseCase {
     ) {}
 
     public async run(userDto: CreateUserDto): Promise<AccessDto> {
+        await this.checkEmail(userDto.email)
         const user = await this.userRepository.create(userDto)
         const fullToken = await this.authRepository.createToken(user)
         const { token, type } = fullToken.toJSON()
         return { token, type, id: user.id }
+    }
+
+    private async checkEmail(email: string): Promise<void> {
+        const user = await this.userRepository.getByEmail(email)
+        if(user !== null) {
+            throw new ApplicationException('Email inválido', { status: 400 })
+        }
     }
 }
